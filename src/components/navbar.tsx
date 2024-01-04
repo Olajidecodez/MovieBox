7648cc78a2d5faf9297665aa9c9d7dc9fc0f2b65
Olajide Balogun <olajidebalogun@icloud.com>
2024-01-04 10:58:24 +0100

    add links to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,9 +4,22 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
     <Menu fixed="top" size="huge"> 
-      <Menu.Menu position="right">
-        <Menu.Item as={Link} to="/auth"> Auth</Menu.Item>
-      </Menu.Menu>
+
+    <Menu.Item as={Link} to="/" style={{fontSize: "1.5rem"}}> 
+        Home
+    </Menu.Item>
+
+    <Menu.Item as={Link} to="/rated" style={{fontSize: "1.5rem"}}> 
+        Rated
+    </Menu.Item>
+
+
+    <Menu.Menu position="right">
+      <Menu.Item as={Link} to="/auth" style={{fontSize:  "1.5rem"}}> 
+        Auth
+      </Menu.Item>
+  </Menu.Menu>
+
     </Menu>
   );
 };
