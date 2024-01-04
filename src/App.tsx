43bbfb171030b22916563c554d6188d85986cfdb
Olajide Balogun <olajidebalogun@icloud.com>
2024-01-04 10:35:18 +0100

    Add Routes for every page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/auth" element={<h1>Auth</h1>} />
+          <Route path="/rated" element={<h1>Rated Page</h1>} />
         </Routes>
       </Router>
     </div>
