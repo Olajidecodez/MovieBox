8a060607ed742ace7fcf8c5232477fc0b0299bb8
Olajide Balogun <olajidebalogun@icloud.com>
2024-01-10 23:51:38 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from "./components/navbar";
 import { Auth } from "./pages/auth";
+import { Home } from "./pages/home";
 import './App.css'
 
 
@@ -10,7 +11,7 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/" element={<Home/>} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/rated" element={<h1>Rated Page</h1>} />
         </Routes>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,21 @@
-export const Home = () => {
+import { Button } from 'semantic-ui-react';
+import { useState } from 'react';
 
+enum DisplayType {
+  Movies = "movies",
+  TvShows = "tvshows"
+}
+export const Home = () => {
+  const [displayType, setDisplayType] = useState<DisplayType>(
+    DisplayType.Movies
+  )
+    
+  return(
+    <div style={{ marginTop: 30, height: "auto"}}>
+       <Button.Group> 
+          <Button color={displayType === DisplayType.Movies ? "blue" : undefined} onClick={() => setDisplayType(DisplayType.Movies)}>Movies </Button>
+          <Button color={displayType === DisplayType.TvShows ? "blue" : undefined} onClick={() => setDisplayType(DisplayType.TvShows)}> TvShows</Button>
+       </Button.Group>
+    </div>
+  );
 }; 
